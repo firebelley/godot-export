40d6b0c1fc0ef8b872d546f93ab410234e646ddb
firebelley <firebelley@users.noreply.github.com>
2019-12-24 11:00:14 -0500

    changes to config checks

--- src/main.ts
+++ src/main.ts
@@ -12,40 +12,43 @@ const relativeProjectPath = core.getInput('relative_project_path');
 const githubClient = new github.GitHub(process.env['GITHUB_TOKEN'] ?? '');
 
 async function main(): Promise<number> {
-  if (!process.env['GITHUB_TOKEN']) {
-    core.error('You must supply the GITHUB_TOKEN environment variable.');
-    return 1;
-  }
-
-  if (!hasExportPresets()) {
-    core.error(
-      'No "export_presets.cfg" found. Please be sure you have defined at least 1 export from the Godot editor.',
-    );
-    return 1;
-  }
-
-  const newVersion = await getNewVersion();
-  if (!newVersion) {
-    core.error(
-      'Could not establish a version for the release. Please check that "base_version" is a https://semver.org/ style string.',
-    );
-    return 1;
-  }
+  await configCheck();
+  const newVersion = await getAndCheckNewVersion();
 
   core.info(`Using release version ${newVersion.format()}`);
 
   await setupWorkingPath();
-  await core.group('Godot setup', setupDependencies).catch(logAndExit);
+  await core.group('Godot setup', setupDependencies);
 
-  const exportResults = await core.group('Exporting', runExport).catch(logAndExit);
+  const exportResults = await core.group('Exporting', runExport);
   if (exportResults) {
-    await core
-      .group(`Create release ${newVersion.format()}`, async () => {
-        await createRelease(newVersion, exportResults);
-      })
-      .catch(logAndExit);
+    await core.group(`Create release ${newVersion.format()}`, async () => {
+      await createRelease(newVersion, exportResults);
+    });
   }
   return 0;
+}
+
+async function configCheck(): Promise<void> {
+  if (!process.env['GITHUB_TOKEN']) {
+    throw new Error('You must supply the GITHUB_TOKEN environment variable.');
+  }
+
+  if (!hasExportPresets()) {
+    throw new Error(
+      'No "export_presets.cfg" found. Please be sure you have defined at least 1 export from the Godot editor.',
+    );
+  }
+}
+
+async function getAndCheckNewVersion(): Promise<semver.SemVer> {
+  const newVersion = await getNewVersion();
+  if (!newVersion) {
+    throw new Error(
+      'Could not establish a version for the release. Please check that "base_version" is a https://semver.org/ style string.',
+    );
+  }
+  return newVersion;
 }
 
 async function setupWorkingPath(): Promise<void> {
@@ -90,6 +93,6 @@ function logAndExit(error: Error): void {
   process.exit(1);
 }
 
-main();
+main().catch(logAndExit);
 
 export { actionWorkingPath, godotTemplateVersion, relativeProjectPath, githubClient };
