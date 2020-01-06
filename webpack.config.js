20af19f7ab3698ce64c50939c76b4d0f7fdd7ae8
firebelley <firebelley@users.noreply.github.com>
2020-01-06 17:58:39 -0500

    webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
+
 module.exports = {
   target: 'node',
+  mode: 'production',
+  entry: './lib/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
 };
