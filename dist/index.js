d5958ec297e5cae23330d1ba06851d857557efa0
Firebelley <firebelley@users.noreply.github.com>
2022-07-01 19:02:06 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -4576,7 +4576,6 @@ async function moveBuildsToExportDirectory(buildResults, moveArchived) {
 
 
 
-
 async function main() {
     const buildResults = await exportBuilds();
     if (!buildResults.length) {
@@ -4589,7 +4588,7 @@ async function main() {
     if (RELATIVE_EXPORT_PATH || USE_PRESET_EXPORT_PATH) {
         await moveBuildsToExportDirectory(buildResults, ARCHIVE_OUTPUT);
     }
-    core.setOutput('buildDirectory', RELATIVE_EXPORT_PATH ? external_path_default().resolve(RELATIVE_EXPORT_PATH) : GODOT_BUILD_PATH);
+    core.setOutput('buildDirectory', GODOT_BUILD_PATH);
     core.setOutput('archiveDirectory', GODOT_ARCHIVE_PATH);
     return 0;
 }
