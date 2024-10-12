29965918cc35b77c465839035775e0e12dc87029
firebelley <4622925+firebelley@users.noreply.github.com>
2024-10-12 09:42:45 -0400

    v6.0.0 (#136)
    * minor updates
    
    * update documentation
    
    ---------
    
    Co-authored-by: firebelley <firebelley@users.noreply.github.com>

--- src/godot.ts
+++ src/godot.ts
@@ -246,11 +246,11 @@ async function getGodotVersion(): Promise<string> {
   version = versionLines.pop() || 'unknown';
   version = version.trim();
   const regex = /(\d+(\.\d+)+\.\w+(\.mono)?)/;
-  const match = version.match(regex);    
+  const match = version.match(regex);
   if (match) {
-      version = match[1];
+    version = match[1];
   } else {
-      throw new Error('Godot version could not be determined.');
+    throw new Error('Godot version could not be determined.');
   }
 
   return version;
